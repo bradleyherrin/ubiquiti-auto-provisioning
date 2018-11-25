bcd856e612527380d30c94c9b463e79a63bb9ee8
bradleyherrin <bherrin26@gmail.com>
2018-11-25 14:49:55 -0500

    Cleaned up existing code

--- ubiquiti-auto-provision.py
+++ ubiquiti-auto-provision.py
@@ -1,59 +1,62 @@
 #!/usr/bin/env python
 
 # ubiquiti-auto-provision.py
-# This project is authored by Bradley Herrin & covered under the GPL-3.0 license.
-# The goal of this project is to use Python to auto-provision Ubiquiti EdgeRouters and EdgeSwitches.
-# View the full project on GitHub - https://github.com/bradleyherrin/ubiquiti-auto-provisioning
+# This project is authored by Bradley Herrin.
+# It is covered under the GPL-3.0 license.
+# The goal of this project is to use Python to
+# auto-provision Ubiquiti EdgeRouters and EdgeSwitches.
+# View the full project on GitHub
+# https://github.com/bradleyherrin/ubiquiti-auto-provisioning
 
+# Lib import
 import os
 import sys
 import telnetlib
 
-PINGING = True
-ROUTER = "192.168.1.1"
-SWITCH = "192.168.1.2"
-ROUTERPING = os.system("ping -c 1 " + ROUTER)
-SWITCHPING = os.system("ping -c 1 " + SWITCH)
-CREDS = "ubnt"
-SWITCHTFTP = "copy tftp://192.168.1.199/ES-eswh.v1.7.4.5075842.stk backup\n"
+# Variables and functions
+pinging = True
+router = "192.168.1.1"
+switch = "192.168.1.2"
+router_ping = os.system("ping -c 1 " + router)
+switch_ping = os.system("ping -c 1 " + switch)
+creds = "ubnt"
+switch_tftp = "copy tftp://192.168.1.199/ES-eswh.v1.7.4.5075842.stk backup\n"
 
-# Ping check
-print ("Searching for devices...")
-while PINGING:
-    if ROUTERPING == 0:
-        print("Under Construction")
+def provision_router():
+    print('Under Construction')
 
-    elif SWITCHPING == 0:
-        print ("Checking firmware...")
-        tn = telnetlib.Telnet(SWITCH)
+def provision_switch():
+    tn = telnetlib.Telnet(switch)
 
-        # Log into the switch
-        tn.read_until("User: ")
-        tn.write(CREDS + "\n")
-        tn.read_until("Password: ")
-        tn.write(CREDS + "\n")
+    # Log into the switch
+    tn.read_until("User: ")
+    tn.write(creds + "\n")
+    tn.read_until("Password: ")
+    tn.write(creds + "\n")
 
-        # Firmware check
-        tn.write("enable\n")
-        tn.write(CREDS + "\n")
-        tn.write("show bootvar\n")
+    # Firmware check
+    tn.write("enable\n")
+    tn.write(creds + "\n")
+    tn.write("show bootvar\n")
+    if "*1.7.4.5075842":
 
-        # Write config or update firmware
-        if *1.7.4.5075842:
-            print ("Writing base configuration...")
-            tn.write("baseconfig")
-
-        else:
-            # Update firmware
-            print ("Upgrading Firmware...")
-            tn.write(SWITCHTFTP)
-            tn.read_until("(y/n)")
-            tn.write("y\n")
-            tn.read_until("File transfer operation completed sucessfully")
-
-            # Set new firmware as next active and reboot
-            tn.write("boot system backup\n")
-            print("Rebooting switch...")
-            tn.write("reload\n")
+        # Write config
+        tn.write("baseconfig")
     else:
-        print ("Still searching for devices")
+
+        # Update firmware
+        tn.write(switch_tftp)
+        tn.read_until("(y/n)")
+        tn.write("y\n")
+        tn.read_until("File transfer operation completed sucessfully")
+
+        # Set new firmware as next active and reboot
+        tn.write("boot system backup\n")
+        tn.write("reload\n")
+
+# Provisioning script
+while pinging:
+    if router_ping == 0:
+        provision_router()
+    else switch_ping == 0:
+        provision_switch()
