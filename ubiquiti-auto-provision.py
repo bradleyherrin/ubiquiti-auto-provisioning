a08af6b16f924030cfabf6e4746731a8d1e92c65
bradleyherrin <bherrin26@gmail.com>
2018-11-27 17:02:40 -0500

    Added extra breaks to the while loop

--- ubiquiti-auto-provision.py
+++ ubiquiti-auto-provision.py
@@ -111,12 +111,15 @@ welcome_message()
 while pinging:
     if os.system(ping + router + ping_match) == 0:
         # Provision router
-        provision_router()
+        break
+        print("Under Construction")
     elif os.system(ping + switch + ping_match) == 0:
         tn = telnetlib.Telnet(switch)
         # Provision switch
         provision_switch()
+        break
+        print("The switch has been configured!")
     else:
         # No devices found
-        print('No devices found.')
         break
+        print('No devices found.')
