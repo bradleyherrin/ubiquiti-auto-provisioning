763d192f7f7f313c7e3dfca98e1484281eb0a521
Brad Herrin <brad.herrin@corp.atcbb.co>
2018-11-28 21:26:32 -0500

    Initial commit.

---
+++ switch_config.py
@@ -0,0 +1,215 @@
+# switch_config.py
+# This project is authored by Bradley Herrin and Josh Moore.
+# It is covered under the GPL-3.0 license.
+# The goal of this project is to use Python to
+# auto-provision Ubiquiti EdgeMax and AirMax products.
+# View the full project on GitHub
+# https://github.com/bradleyherrin/ubiquiti-auto-provisioning
+
+# Use this section to build your own
+# configs using tn.sendline and tn.expect.
+# Currently all this does is add a new
+# user, remove the default user "ubnt",
+# set up UNMS, and then close the Telnet
+# session. The idea is that you would build
+# out configs for each model you need to
+# provision. Scroll down to see the logic
+# used to check for different switch models.
+
+def switch8():
+    tn.sendline("username " + new_user + "password " + new_pass + priv)
+    tn.expect("#")
+    tn.sendline("exit")
+    tn.expect("#")
+    tn.sendline("exit")
+    tn.expect(">")
+    tn.sendline("exit")
+    switch_new_login()
+    tn.sendline("no username ubnt")
+    tn.expect("#")
+    tn.sendline("service unms key" + unms_key)
+    tn.expect("#")
+    tn.sendline("service unms")
+    tn.expect("#")
+    tn.sendline("exit")
+    tn.expect(">")
+    tn.sendline("exit")
+
+def switch16():
+    tn.sendline("username " + new_user + "password " + new_pass + priv)
+    tn.expect("#")
+    tn.sendline("exit")
+    tn.expect("#")
+    tn.sendline("exit")
+    tn.expect(">")
+    tn.sendline("exit")
+    switch_new_login()
+    tn.sendline("no username ubnt")
+    tn.expect("#")
+    tn.sendline("service unms key" + unms_key)
+    tn.expect("#")
+    tn.sendline("service unms")
+    tn.expect("#")
+    tn.sendline("exit")
+    tn.expect(">")
+    tn.sendline("exit")
+
+def switch24():
+    tn.sendline("username " + new_user + "password " + new_pass + priv)
+    tn.expect("#")
+    tn.sendline("exit")
+    tn.expect("#")
+    tn.sendline("exit")
+    tn.expect(">")
+    tn.sendline("exit")
+    switch_new_login()
+    tn.sendline("no username ubnt")
+    tn.expect("#")
+    tn.sendline("service unms key" + unms_key)
+    tn.expect("#")
+    tn.sendline("service unms")
+    tn.expect("#")
+    tn.sendline("exit")
+    tn.expect(">")
+    tn.sendline("exit")
+
+def switch48():
+    tn.sendline("username " + new_user + "password " + new_pass + priv)
+    tn.expect("#")
+    tn.sendline("exit")
+    tn.expect("#")
+    tn.sendline("exit")
+    tn.expect(">")
+    tn.sendline("exit")
+    switch_new_login()
+    tn.sendline("no username ubnt")
+    tn.expect("#")
+    tn.sendline("service unms key" + unms_key)
+    tn.expect("#")
+    tn.sendline("service unms")
+    tn.expect("#")
+    tn.sendline("exit")
+    tn.expect(">")
+    tn.sendline("exit")
+
+def switch5():
+    tn.sendline("username " + new_user + "password " + new_pass + priv)
+    tn.expect("#")
+    tn.sendline("exit")
+    tn.expect("#")
+    tn.sendline("exit")
+    tn.expect(">")
+    tn.sendline("exit")
+    switch_new_login()
+    tn.sendline("no username ubnt")
+    tn.expect("#")
+    tn.sendline("service unms key" + unms_key)
+    tn.expect("#")
+    tn.sendline("service unms")
+    tn.expect("#")
+    tn.sendline("exit")
+    tn.expect(">")
+    tn.sendline("exit")
+
+def switch10():
+    tn.sendline("username " + new_user + "password " + new_pass + priv)
+    tn.expect("#")
+    tn.sendline("exit")
+    tn.expect("#")
+    tn.sendline("exit")
+    tn.expect(">")
+    tn.sendline("exit")
+    switch_new_login()
+    tn.sendline("no username ubnt")
+    tn.expect("#")
+    tn.sendline("service unms key" + unms_key)
+    tn.expect("#")
+    tn.sendline("service unms")
+    tn.expect("#")
+    tn.sendline("exit")
+    tn.expect(">")
+    tn.sendline("exit")
+
+def switch12():
+    tn.sendline("username " + new_user + "password " + new_pass + priv)
+    tn.expect("#")
+    tn.sendline("exit")
+    tn.expect("#")
+    tn.sendline("exit")
+    tn.expect(">")
+    tn.sendline("exit")
+    switch_new_login()
+    tn.sendline("no username ubnt")
+    tn.expect("#")
+    tn.sendline("service unms key" + unms_key)
+    tn.expect("#")
+    tn.sendline("service unms")
+    tn.expect("#")
+    tn.sendline("exit")
+    tn.expect(">")
+    tn.sendline("exit")
+
+def switchS16():
+    tn.sendline("username " + new_user + "password " + new_pass + priv)
+    tn.expect("#")
+    tn.sendline("exit")
+    tn.expect("#")
+    tn.sendline("exit")
+    tn.expect(">")
+    tn.sendline("exit")
+    switch_new_login()
+    tn.sendline("no username ubnt")
+    tn.expect("#")
+    tn.sendline("service unms key" + unms_key)
+    tn.expect("#")
+    tn.sendline("service unms")
+    tn.expect("#")
+    tn.sendline("exit")
+    tn.expect(">")
+    tn.sendline("exit")
+
+def switch16XG():
+    tn.sendline("username " + new_user + "password " + new_pass + priv)
+    tn.expect("#")
+    tn.sendline("exit")
+    tn.expect("#")
+    tn.sendline("exit")
+    tn.expect(">")
+    tn.sendline("exit")
+    switch_new_login()
+    tn.sendline("no username ubnt")
+    tn.expect("#")
+    tn.sendline("service unms key" + unms_key)
+    tn.expect("#")
+    tn.sendline("service unms")
+    tn.expect("#")
+    tn.sendline("exit")
+    tn.expect(">")
+    tn.sendline("exit")
+
+# Here is the logic that will check for which config to serve.
+# Keep in mind that the order is important. For example, the
+# ES-16-XG comes before ES-16 switch to prevent the switch16()
+# function from being pushed to an ES-16-XG.
+
+def switch_config():
+    tn.sendline("show version")
+    tn.expect("Serial")
+    if "ES-8" in tn.before:
+        switch8()
+    elif "ES-16-XG" in tn.before:
+        switch16XG()
+    elif "ES-16" in tn.before:
+        switch16()
+    elif "ES-24" in tn.before:
+        switch24()
+    elif "ES-48" in tn.before:
+        switch48()
+    elif "ES-5XP" in tn.before:
+        switch5()
+    elif "ES-10" in tn.before:
+        switch10()
+    elif "ES-12" in tn.before:
+        switch12()
+    elif "EP-S16" in tn.before:
+        switchS16()
