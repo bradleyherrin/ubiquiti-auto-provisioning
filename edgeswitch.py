965aa47c51a3bfa4bd9a3a72f00c11a3831e01b2
Moore <jjsmo@f45c89a576d5.ant.amazon.com>
2018-12-09 01:00:40 -0800

    fixing find syntax

--- edgeswitch.py
+++ edgeswitch.py
@@ -149,6 +149,7 @@ def latest_switch_firmware(hardcoded_switch_version, firmware_path, model):
     firmware = switch_models.get(model, {}).get('firmware')
     if firmware_path and not hardcoded_switch_version:
         firmware_files = pexpect.run("find " + firmware_path + " -printf '%f\n' | grep stk | grep " + firmware)
+        print firmware_files
         split_version = firmware_files.rsplit(firmware)
         switch_version = split_version.split('.stk')
         return switch_version
